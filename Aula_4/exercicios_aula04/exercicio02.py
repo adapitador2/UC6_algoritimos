2a8dbd20fbd9ffb38e373a2aaa40f08b4452ff62
adapitador2 <pedromamao05@gmail.com>
2026-02-23 21:24:00 -0300

    aula 07 e testes blublublu

--- Aula_4/exercicios_aula04/exercicio02.py
+++ Aula_4/exercicios_aula04/exercicio02.py
@@ -1,28 +1,11 @@
-num1 = int(input("digite um numero: "))
+num1 = int(input("numero 1"))
+num2 = int(input("numero 2"))
 
-if num1 == 1:
-    print("um")
-elif num1 == 2:
-    print("dois")
-elif num1 == 3:
-    print("tres")
-elif num1 == 4:
-    print("quatro")
-elif num1 == 5:
-    print("cinco")
-elif num1 == 6:
-    print("seis")
-elif num1 == 7:
-    print("sete")
-elif num1 == 8:
-    print("oito")
-elif num1 == 9:
-    print("nove")
-elif num1 == 10:
-    print("dez")
-elif num1 == 11:
-    print("onze")
-elif num1 == 12:
-    print("doze")
+if num1 > num2:
+    print(num1, "é maior que ", num2)
+
+elif num1 == num2:
+    print("os numeros são iguais")
+
 else:
-    print("so vai até o 12")
+    print(num2, "é maior que ", num1)
--- Aula_4/exercicios_aula04/exercicio03.py
+++ Aula_4/exercicios_aula04/exercicio03.py
@@ -1,20 +1,28 @@
-"""
-num1 = int(input("numero: "))
+num1 = int(input("digite um numero: "))
 
-resultado = num1 % 2
-
-if resultado == 0:
-    print(num1, "é par")
-
+if num1 == 1:
+    print("um")
+elif num1 == 2:
+    print("dois")
+elif num1 == 3:
+    print("tres")
+elif num1 == 4:
+    print("quatro")
+elif num1 == 5:
+    print("cinco")
+elif num1 == 6:
+    print("seis")
+elif num1 == 7:
+    print("sete")
+elif num1 == 8:
+    print("oito")
+elif num1 == 9:
+    print("nove")
+elif num1 == 10:
+    print("dez")
+elif num1 == 11:
+    print("onze")
+elif num1 == 12:
+    print("doze")
 else:
-    print(num1, "é impar")
-"""
-
-
-num1 = int(input("numero: "))
-
-if num1 % 2 == 0:
-    print("par")
-
-else: 
-    print("impar")
+    print("so vai até o 12")
