fa316600dc192c349d0215e7b16acb13df39a336
adapitador2 <pedromamao05@gmail.com>
2026-03-09 22:17:09 -0300

    aula 11 (api)

--- Aula_11/exercicios/exercicio_13.py
+++ Aula_11/exercicios/exercicio_13.py
@@ -2,7 +2,7 @@
 
 import random
 
-num_usuario = int(input("digite um numero de 1 a 10: "))
+num_usuario = int(input("digite um numero de 1 a 2000: "))
 num_random = random.randint(1,2000)
 contador = 0
 
@@ -12,8 +12,8 @@ print(num_random)
 while True:
     if num_usuario != num_random:
         print("perdeu otario")
-        num_usuario = random.randint(1,2000) #int(input("digite novamente: "))
-        num_random = random.randint(1,2000)
+        num_usuario = random.randint(1,20000) #int(input("digite novamente: "))
+        num_random = random.randint(1,20000)
         contador += 1
         print(num_usuario)
         print(num_random)
